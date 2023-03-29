81721eba1ec8580c3cc73af8c398b350799772e0
AmakoveBridgit <amakovebridgit@gmail.com>
2023-03-29 19:27:43 +0300

    python

---
+++ functions/hello.py
@@ -0,0 +1,5 @@
+def hello(name):
+     print("hello {name}")
+
+def welcome(student,school):
+     print(f"welcome to {school} {student} ")    
---
+++ functions/math.py
@@ -0,0 +1,29 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtract(c,d):
+    answer=c-d
+    return answer
+
+
+def divide(x,t):
+    answer=x/t
+    return answer
+
+
+
+def multiply(m,w):
+    answer=m*w
+    return answer
+
+
+
+def remainder(y,k):
+    answer=y%k
+    return answer
+
+
+
+
+
